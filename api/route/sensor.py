9ed1114f26822b519b029f2c5c91fad2b514064b
Faizgeeky <nandoliyafaiz429@gmail.com>
2024-07-28 22:29:11 +0530

    data retrival and inject done

--- api/route/sensor.py
+++ api/route/sensor.py
@@ -7,6 +7,6 @@ sensor_bp = Blueprint('sensor',__name__)
 
 
 data_view = SensorDataAPI.as_view('data_api')
-sensor_bp.add_url_rule('/data/', defaults={'id': None}, view_func=data_view, methods=['GET'])
-sensor_bp.add_url_rule('/data/', view_func=data_view, methods=['POST'])
-sensor_bp.add_url_rule('/data/<int:id>', view_func=data_view, methods=['GET', 'DELETE'])
+# sensor_bp.add_url_rule('/data/', defaults={'id': None}, view_func=data_view, methods=['GET'])
+sensor_bp.add_url_rule('/data/', view_func=data_view, methods=['GET','POST'])
+sensor_bp.add_url_rule('/data/<string:sensor_id>', view_func=data_view, methods=['GET', 'DELETE'])
--- api/route/views.py
+++ api/route/views.py
@@ -11,42 +11,75 @@ sensor_schema = SensorSchema(many=True)
 
 
 class SensorDataAPI(MethodView):
-
     # Handle filter args dynamically
-    def filter_sensor_Data(self,args):
+    def filter_sensor_Data(self,header_agrs, **args):
+        # print(args)
         filters = []
 
+      
         if 'sensor_id' in args:
-            print("here")
-            filters.append(Sensor.sensor_id == args.get('sensor_id', type=str))
-        if 'start_time' in args:
-            filters.append(Sensor.timestamp >= args['start_time'])
-        if 'end_time' in args:
-            filters.append(Sensor.timestamp <= args['end_time'])
-        if 'min_humidity' in args:
-            filters.append(Sensor.humidity >= args['min_humidity'])
-        if 'max_humidity' in args:
-            filters.append(Sensor.humidity <= args['max_humidity'])
-        if 'min_pressure' in args:
-            filters.append(Sensor.pressure >= args['min_pressure'])
-        if 'max_pressure' in args:
-            filters.append(Sensor.pressure <= args['max_pressure'])
-        if 'min_temperature' in args:
-            filters.append(Sensor.temperature >= args.get('min_temperature', type=float))
-        if 'max_temperature' in args:
-            filters.append(Sensor.temperature <= args.get('max_temperature', type=float))
+            filters.append(Sensor.sensor_id == str(args['sensor_id']))
+        if 'start_time' in header_agrs:
+            filters.append(Sensor.timestamp >= header_agrs['start_time'])
+        if 'end_time' in header_agrs:
+            filters.append(Sensor.timestamp <= header_agrs['end_time'])
+        if 'min_humidity' in header_agrs:
+            filters.append(Sensor.humidity >= header_agrs['min_humidity'])
+        if 'max_humidity' in header_agrs:
+            filters.append(Sensor.humidity <= header_agrs['max_humidity'])
+        if 'min_pressure' in header_agrs:
+            filters.append(Sensor.pressure >= header_agrs['min_pressure'])
+        if 'max_pressure' in header_agrs:
+            filters.append(Sensor.pressure <= header_agrs['max_pressure'])
+        if 'min_temperature' in header_agrs:
+            filters.append(Sensor.temperature >= header_agrs.get('min_temperature', type=float))
+        if 'max_temperature' in header_agrs:
+            filters.append(Sensor.temperature <= header_agrs.get('max_temperature', type=float))
         
         # print("filters", filters) //checkpoint
         # return sql filtered obj
         return filters
     
+    def aggregration(self, df, aggregate_type):
+        print("types are", aggregate_type)
+        # applyiung aggregation
+        if len(df) >0 and 'hourly' in aggregate_type and 'daily' in aggregate_type:
+            print("Dataframe is", df)
+            df['timestamp'] = pd.to_datetime(df['timestamp'])
+            df = df[['timestamp','temperature','humidity', 'pressure']]
+            df_hour = df.groupby(df['timestamp'].dt.hour).mean()
+            df_day = df.groupby(df['timestamp'].dt.day).mean()
 
-    def get(self, id=None):
-        if id is None:
+            hourly_data = df_hour.to_dict(orient='records')
+            daily_data = df_day.to_dict(orient='records')
+
+            return {'daily_avg':daily_data,'hourly_avg':hourly_data}
+
+        elif len(df) >0 and 'hourly' in aggregate_type:
+            df['timestamp'] = pd.to_datetime(df['timestamp'])
+            df = df[['timestamp','temperature','humidity', 'pressure']]
+            df_hour = df.groupby(df['timestamp'].dt.hour).mean()
+            
+            print("Datafrane mean is ", df_hour)
+            hourly_data = df_hour.to_dict(orient='records')
+
+            return {'hourly_avg':hourly_data}
+
+        elif len(df) >0 and 'daily' in aggregate_type:
+            df['timestamp'] = pd.to_datetime(df['timestamp'])
+            df = df[['timestamp','temperature','humidity', 'pressure']]
+            df_day = df.groupby(df['timestamp'].dt.day).mean()
+            daily_data = df_day.to_dict(orient='records')
+
+            # Return as JSON response
+            return {'daily_avg':daily_data}
+        
+    def get(self, sensor_id=None):
+        if sensor_id is None:
             json_data = request.args
             page = int(request.args.get('page', 1))
             per_page = int(request.args.get('per_page', 10))
-            aggregate = request.args.get('aggregate')
+            
             # Handle dynamic args to filter
             filters = self.filter_sensor_Data(json_data)
             # retured filterd sqlalchemy obj 
@@ -59,46 +92,18 @@ class SensorDataAPI(MethodView):
             result = sensor_schema.dump(sensor_data)
             df = pd.DataFrame(result)
 
-            def aggregration(df, aggregate_type):
-                print("types are", aggregate_type)
-                # applyiung aggregation
-                if 'hourly' in aggregate_type and 'daily' in aggregate_type:
-                    df['timestamp'] = pd.to_datetime(df['timestamp'])
-                    print("Dataframe is", df)
-                    df = df[['timestamp','temperature','humidity', 'pressure']]
-                    df_hour = df.groupby(df['timestamp'].dt.hour).mean()
-                    df_day = df.groupby(df['timestamp'].dt.day).mean()
-
-                    hourly_data = df_hour.to_dict(orient='records')
-                    daily_data = df_day.to_dict(orient='records')
-
-                    return {'daily_avg':daily_data,'hourly_avg':hourly_data}
-
-                elif 'hourly' in aggregate_type:
-                    df['timestamp'] = pd.to_datetime(df['timestamp'])
-                    df = df[['timestamp','temperature','humidity', 'pressure']]
-                    df_hour = df.groupby(df['timestamp'].dt.hour).mean()
-                    
-                    print("Datafrane mean is ", df_hour)
-                    hourly_data = df_hour.to_dict(orient='records')
-
-                    return {'hourly_avg':hourly_data}
-
-                elif 'daily' in aggregate_type:
-                    df['timestamp'] = pd.to_datetime(df['timestamp'])
-                    df = df[['timestamp','temperature','humidity', 'pressure']]
-                    df_day = df.groupby(df['timestamp'].dt.day).mean()
-                    daily_data = df_day.to_dict(orient='records')
-
-                    # Return as JSON response
-                    return {'daily_avg':daily_data}
-
+            
             # If aggregation requested return aggregate data only
+            aggregate = request.args.get('aggregate')
             if aggregate != None:
-                result = aggregration(df, aggregate)
-                return jsonify({
-                        'aggregate': result,
-                    }), HTTPStatus.OK
+                result = self.aggregration(df, aggregate)
+                if result is not None :
+                    return jsonify({
+                            'aggregate': result,
+                        }), HTTPStatus.OK
+                else:
+                    return jsonify({"message":"No data found"}), HTTPStatus.OK
+            
             # else return all sensor data
             else:
                 return jsonify({
@@ -113,62 +118,31 @@ class SensorDataAPI(MethodView):
                     }
                 }), HTTPStatus.OK
         else:
+            # print("id is", id)
             try:
                 json_data = request.args
                 page = int(request.args.get('page', 1))
                 per_page = int(request.args.get('per_page', 10))
-                aggregate = request.args.get('aggregate')
                 # Handle dynamic args to filter
-                filters = self.filter_sensor_Data(json_data)
+                filters = self.filter_sensor_Data(json_data, sensor_id = sensor_id)
+
                 # retured filterd sqlalchemy obj 
                 query = Sensor.query.filter(*filters)
-                
-                
                 pagination = query.paginate(page=page, per_page=per_page, error_out=False)
-                
                 sensor_data = pagination.items
                 result = sensor_schema.dump(sensor_data)
                 df = pd.DataFrame(result)
 
-                def aggregration(df, aggregate_type):
-                    print("types are", aggregate_type)
-                    # applyiung aggregation
-                    if 'hourly' in aggregate_type and 'daily' in aggregate_type:
-                        df['timestamp'] = pd.to_datetime(df['timestamp'])
-                        df = df[['timestamp','temperature','humidity', 'pressure']]
-                        df_hour = df.groupby(df['timestamp'].dt.hour).mean()
-                        df_day = df.groupby(df['timestamp'].dt.day).mean()
-
-                        hourly_data = df_hour.to_dict(orient='records')
-                        daily_data = df_day.to_dict(orient='records')
-
-                        return {'daily_avg':daily_data,'hourly_avg':hourly_data}
-
-                    elif 'hourly' in aggregate_type:
-                        df['timestamp'] = pd.to_datetime(df['timestamp'])
-                        df = df[['timestamp','temperature','humidity', 'pressure']]
-                        df_hour = df.groupby(df['timestamp'].dt.hour).mean()
-                        
-                        print("Datafrane mean is ", df_hour)
-                        hourly_data = df_hour.to_dict(orient='records')
-
-                        return {'hourly_avg':hourly_data}
-
-                    elif 'daily' in aggregate_type:
-                        df['timestamp'] = pd.to_datetime(df['timestamp'])
-                        df = df[['timestamp','temperature','humidity', 'pressure']]
-                        df_day = df.groupby(df['timestamp'].dt.day).mean()
-                        daily_data = df_day.to_dict(orient='records')
-
-                        # Return as JSON response
-                        return {'daily_avg':daily_data}
-
                 # If aggregation requested return aggregate data only
+                aggregate = request.args.get('aggregate')
                 if aggregate != None:
-                    result = aggregration(df, aggregate)
-                    return jsonify({
-                            'aggregate': result,
-                        }), HTTPStatus.OK
+                    result = self.aggregration(df, aggregate)
+                    if result is not None :
+                        return jsonify({
+                                'aggregate': result,
+                            }), HTTPStatus.OK
+                    else:
+                        return jsonify({"message":"No data found"}), HTTPStatus.OK
                 # else return all sensor data
                 else:
                     return jsonify({
@@ -205,6 +179,6 @@ class SensorDataAPI(MethodView):
             return jsonify({"message": "Invalid request", "error": str(e)}), HTTPStatus.INTERNAL_SERVER_ERROR
             
 
-    def delete(self, id):
+    def put(self, sensor_id):
         # Handle deletion of data with the given id
-        return jsonify({"message": f"Data with id {id} deleted"})
+        return jsonify({"message": f"Data with id {sensor_id} deleted"})
